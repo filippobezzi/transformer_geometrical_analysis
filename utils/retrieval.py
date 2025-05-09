edd58b49517415564f597d205154a6d38a0f5be4
edoardo-damore <edoardododo.01@gmail.com>
2025-05-09 16:13:51 +0200

    added .gitcommit and improved retrieval.py

--- utils/retrieval.py
+++ utils/retrieval.py
@@ -1,24 +1,32 @@
 def get_activations(model):
+    """
+    Function that attaches hooks that save the activations of the model
+    Args:
+        model (GPT2LMHeadModel): the gpt2 model in which hook functions will be attached
+    Returns:
+        activations (dict): empty dictionary where the activations of attention and mlp will be stored 
+    """
 
     # dict to store all activations
-    #   both from attention and from mlp
     activations = {
         "attn_inputs": [],
+        "attn_attentions": [], # this will have to be saved manually from output.activations
         "attn_outputs": [],
         "mlp_inputs": [],
         "mlp_outputs": []
     }
 
-    # Hook functions to save the activations
+    # hook functions to save the activations
     def hook_attention(module, input, output):
         # input: (hidden_states,) | output: hidden_states after attention
-        activations["attn_inputs"].append(input)
+        activations["attn_inputs"].append(input[0][0])
         # we save the embedded vectors as a whole and not partitioned as they have been processed by the heads
-        activations["attn_outputs"].append(output[0]) 
+        activations["attn_outputs"].append(output[0][0]) 
 
+    # same for the mlp
     def hook_mlp(module, input, output):
-        activations["mlp_inputs"].append(input)
-        activations["mlp_outputs"].append(output)
+        activations["mlp_inputs"].append(input[0][0])
+        activations["mlp_outputs"].append(output[0])
 
     # we now register the hooks so that at each block they are called
     for i, block in enumerate(model.transformer.h):

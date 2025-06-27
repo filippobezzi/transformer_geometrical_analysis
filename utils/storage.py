253399c6ec6c7579f0e47d5227f1f9ed21c56d52
edoardo-damore <edoardododo.01@gmail.com>
2025-06-27 15:32:12 +0200

    minor fix

--- utils/storage.py
+++ utils/storage.py
@@ -3,19 +3,19 @@ import pandas as pd
 import torch
 
 
-def save_svals(svals_dict: dict, sigma: np.ndarray, block: str):
+def save_svals(svals_dict: dict, sigma: np.ndarray, block: str, dir: str = "data"):
     df = pd.DataFrame(svals_dict)
     sigmas = pd.DataFrame(sigma, columns=df.columns, index=["sigma"])
     df = pd.concat((sigmas, df))
-    df.to_csv(f"ip/data/svals/{block}.csv")
+    df.to_csv(f"{dir}/svals/{block}.csv")
     return
 
-def save_overlaps(overlaps_dict: dict, block: str):
+def save_overlaps(overlaps_dict: dict, block: str, dir: str = "data"):
     df = pd.DataFrame(overlaps_dict)
-    df.to_csv(f"ip/data/overlaps/{block}.csv")
+    df.to_csv(f"{dir}/overlaps/{block}.csv")
     return
 
-def save_projections(projections_dict: dict, block: str):
+def save_projections(projections_dict: dict, block: str, dir: str = "data"):
     df = pd.DataFrame(projections_dict)
-    df.to_csv(f"ip/data/projections/{block}.csv")
+    df.to_csv(f"{dir}/projections/{block}.csv")
     return
